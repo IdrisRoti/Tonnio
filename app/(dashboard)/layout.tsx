d8e4a3db656f6afcf6951563348af10983296bbd
IdrisRoti <idrisomisakin@gmail.com>
2024-10-17 03:07:51 +0100

    chore: set folder structure

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function DashboardLayout({children}:{children: React.ReactNode}) {
+  return (
+    <div>
+        DashboardLayout
+        {children}
+    </div>
+  )
+}
---
+++ app/(job-application)/layout.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function ApplicationLayout({children}:{children: React.ReactNode}) {
+  return (
+    <div>
+        ApplicationLayout
+        {children}
+    </div>
+  )
+}
